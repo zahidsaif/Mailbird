6217dd66f82b63eba3d164ef9ebc9b1d591af9e8
Zahid Saif <zahid.18dbca@cmr.edu.in>
2021-07-07 11:40:03 +0530

    Added Original Mailbird files

---
+++ config/dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+    googleClientID: "801632878406-d9s00n0cse1dfu4rbfb2uuu2t4md1329.apps.googleusercontent.com",
+    googleClientSecret: "OM0MHl505aXV9r1mBPTHE8y0",
+    mongoURI: 'mongodb+srv://test123:test123@cluster0.fnsak.mongodb.net/Cluster0?retryWrites=true&w=majority',
+    cookieKey: "random_string",
+    stripePublishableKey: 'pk_test_51J9DzZSIDJ7RrqcksjdA2zdWOeIR6dqnOHJZRaqZh6k1PNhLAkJfsoGl2f7EgUylvyRLbf0VOt0F3gRe1oAZK6EB00krcX6eKK',
+    stripeSecretKey: 'sk_test_51J9DzZSIDJ7RrqckYe738dAj6X831As2v1bTTn7f7Gga1FnaNfiAUilnE422JUxaJTK6t4u6N0hALkcrnOKWYkIb00dUioltq7',
+    sendGridKey: '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',
+    redirectDomain: 'http://localhost:5000'
+};
